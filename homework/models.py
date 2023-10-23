9df0934b7e439814fbe0d21e72af152019a61c5b
sergeybryazgin <sergeybryazgin@omg>
2023-10-23 23:55:17 +0300

    закончил на remove_product

--- homework/models.py
+++ homework/models.py
@@ -31,7 +31,7 @@ class Product:
             Если продуктов не хватает, то выбросите исключение ValueError
         """
         if Product.check_quantity(self, quantity) is not True:
-            return ValueError("Запрашиваемого продукта нет в таком количестве")
+            raise ValueError("Запрашиваемого продукта нет в таком количестве")
         else:
             return self.quantity - quantity
 
@@ -57,7 +57,10 @@ class Cart:
         Метод добавления продукта в корзину.
         Если продукт уже есть в корзине, то увеличиваем количество
         """
-        raise NotImplementedError
+        if product in self.products:
+            self.products[product] += buy_count
+        else:
+            self.products[product] = buy_count
 
     def remove_product(self, product: Product, remove_count=None):
         """
@@ -65,7 +68,10 @@ class Cart:
         Если remove_count не передан, то удаляется вся позиция
         Если remove_count больше, чем количество продуктов в позиции, то удаляется вся позиция
         """
-        raise NotImplementedError
+        if product in self.products:
+            self.products[product] -= remove_count
+            if remove_count is None or remove_count > self.products[product]
+                self.products[product] = 0
 
     def clear(self):
         raise NotImplementedError
--- homework/test_shop.py
+++ homework/test_shop.py
@@ -3,12 +3,16 @@
 """
 import pytest
 
-from homework.models import Product
+from homework.models import Product, Cart
 
 
 @pytest.fixture
 def product():
     return Product("book", 100, "This is a book", 1000)
+
+@pytest.fixture
+def cart():
+    return Cart()
 
 
 class TestProducts:
@@ -19,25 +23,21 @@ class TestProducts:
 
     def test_product_check_quantity(self, product):
         # TODO напишите проверки на метод check_quantity
-        assert Product.check_quantity(product, 999) == True
-        assert Product.check_quantity(product, 1000) == True
-        assert Product.check_quantity(product, 1001) == False
-        print(Product.check_quantity(product, 1999))
+        assert Product.check_quantity(product, 999)
+        assert Product.check_quantity(product, 1000)
+        assert not Product.check_quantity(product, 1001)
 
     def test_product_buy(self, product):
         # TODO напишите проверки на метод buy
 
         # ПЕРЕДЕЛАТЬ
-
-
         assert Product.buy(product, 999) == 1
-        assert str(Product.buy(product, 12000)) == 'Запрашиваемого продукта нет в таком количестве'
-
 
     def test_product_buy_more_than_available(self, product):
         # TODO напишите проверки на метод buy,
         #  которые ожидают ошибку ValueError при попытке купить больше, чем есть в наличии
-        pass
+        with pytest.raises(ValueError, match='Запрашиваемого продукта нет в таком количестве'):
+            Product.buy(product, 12000)
 
 
 class TestCart:
@@ -47,3 +47,15 @@ class TestCart:
         На некоторые методы у вас может быть несколько тестов.
         Например, негативные тесты, ожидающие ошибку (используйте pytest.raises, чтобы проверить это)
     """
+
+    def test_cart_add_product(self, product, cart):
+        """
+        Проверки на метод add_product
+        """
+        cart.add_product(product, 1)
+        assert cart.products[product] == 1
+        #print(cart.products.values())
+        cart.add_product(product, 2)
+        with pytest.raises(AssertionError):
+            assert cart.products[product] == 4
+        #print(cart.products.values())
